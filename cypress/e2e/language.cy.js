bb9566d7f0c78de0f34142ad05fb42a937c3780d
unknown <you@example.com>
2026-02-19 22:43:21 +0200

    Update project

--- cypress/e2e/language.cy.js
+++ cypress/e2e/language.cy.js
@@ -2,16 +2,25 @@ import app from "../pages/AppPage";
 import map from "../pages/MapPage";
 
 describe("Unifly - Map page", () => {
+
   beforeEach(() => {
+    // Arrange: open app before each test
     app.open();
+
+     // Basic smoke check that we are on the right app
     cy.url().should("include", "unifly");
   });
 
 it('should select Deutsch and update UI', () => {
+
+     // Arrange
     map.interceptLanguageChange()
-    map.dropdownIndicator.should('be.visible').click()
-    map.dropdownMenu.should('be.visible')
+    
+    // Act
+    map.openDropdown()
     map.selectLanguage()
+
+    // Assert
     map.verifyLanguageChangeResponse()
     map.verifyGermanUI() 
 
